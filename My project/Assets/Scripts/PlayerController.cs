f0214ffd0c1eff8c9d2a3394ed19c24db93cd1fe
KevinEbcs <32904488+KevinEbcs@users.noreply.github.com>
2024-05-03 11:02:23 +0200

    Physics Materials etc
    Außerdem Verbesserung der Spielerbewegung

--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -6,36 +6,66 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody _rigidbody;
     
+    
+    private LayerMask ground;
+    private float playerHeight;
+    
+    [SerializeField] private Transform orientation;
+    [Header("Movement")]
     [SerializeField]
     private float movementForce;
-
-    [SerializeField] private Transform orientation;
-
     [SerializeField] private float jumpForce;
+
+    
+
+    
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
         _rigidbody.freezeRotation = true;
+
+        playerHeight = transform.lossyScale.y;
     }
 
     // Update is called once per frame
     void Update()
     {
+        processMovement();
+        
+    }
+
+    private bool groundCheck()
+    {
+        bool isGrounded = Physics.Raycast(transform.position, 
+            Vector3.down, 
+            playerHeight * 0.5f + 0.2f,
+            ground);
+        
+        return isGrounded;
+    }
+
+    private void processMovement()
+    {
         Vector3 movementVector = (orientation.right * Input.GetAxis("Horizontal")
-            + orientation.forward * Input.GetAxis("Vertical")).normalized
-            * movementForce
-            * Time.deltaTime;
+                                  + orientation.forward * Input.GetAxis("Vertical")).normalized
+                                 * movementForce
+                                 * Time.deltaTime;
         
         _rigidbody.AddForce(movementVector, ForceMode.Force);
         
-        if (Input.GetAxis("Jump")>0)
+        if (Input.GetAxis("Jump")>0 && groundCheck())
         {
-           _rigidbody.AddForce(orientation.up * 
-                               jumpForce * 
-                               Time.deltaTime); 
+            
+            _rigidbody.AddForce(orientation.up * 
+                                jumpForce * 
+                                Time.deltaTime); 
         }
-        
+
+        if (Input.GetAxis("Jump") > 0)
+        {
+            Debug.Log(playerHeight);    
+        }
         
     }
 }

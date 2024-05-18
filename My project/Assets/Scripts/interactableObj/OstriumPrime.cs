c09dea4cf12e9963230596223df70fadd276dceb
LennartGillberg <lennart@wp-wartung24.de>
2024-05-18 18:11:50 +0200

    act1

--- My project/Assets/Scripts/interactableObj/OstriumPrime.cs
+++ My project/Assets/Scripts/interactableObj/OstriumPrime.cs
@@ -6,16 +6,43 @@ using UnityEngine;
 
 public class OstriumPrime : DynamicObject
 {
+    private Vector3 startPos;
+    private bool rotate = true;
+    [SerializeField] private float hoverMagnitude = 0.1f;
+    [SerializeField] private float hoverSpeed = 1.5f;
+    [SerializeField] private float rotationSpeed = 1.0f;
+    [SerializeField] private float movementSpeed = 10.0f;
+    [SerializeField] private float distanceToPlayer = 15.0f;
+
+
+    [SerializeField] private GameObject player;
+    private Vector3 initPlayerPos;
     //private bool _interacted = false;
+
     void Start()
     {
         InteractText = "E OstriumPrime";
+        startPos = player.transform.position;
+        startPos.x += distanceToPlayer;
+
+        initPlayerPos = player.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 pos = transform.position;
+        pos.y = startPos.y + Mathf.Cos(Time.time * hoverSpeed) * hoverMagnitude;
+        transform.position = pos;
+
+        transform.Rotate(0.0f, Time.time * rotationSpeed, 0.0f);
+
+        if(rotate){
+            transform.RotateAround(initPlayerPos, Vector3.up, movementSpeed * Time.deltaTime);
+            if((player.transform.position - transform.position).magnitude < 5){
+                rotate = false;
+            }
+        }
     }
     public override void ObjectInteract()
     {

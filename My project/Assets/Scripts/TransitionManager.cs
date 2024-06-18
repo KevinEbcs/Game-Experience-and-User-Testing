4b9e225ae547a13ec4dfdb4d5fd192b3b6a3023d
Marc Lennart Gillberg <marc.gillberg@smail.inf.h-brs.de>
2024-06-18 13:17:39 +0200

    Transition Enter

--- My project/Assets/Scripts/TransitionManager.cs
+++ My project/Assets/Scripts/TransitionManager.cs
@@ -26,7 +26,7 @@ public class TransitionManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Mouse0))
         {
             string nextLevel = gP.GetNextLevel();
             int nextLevelId = gP.GetNextLevelId();
